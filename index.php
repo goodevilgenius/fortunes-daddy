1f79d2db34cd3c8efafa588b23a9f32953c92c11
Dan Jones <good.evil.genius@gmail.com>
2013-04-25 15:18:14 -0400

    Got rid of line breaks, and long whitespace

--- index.php
+++ index.php
@@ -5,5 +5,7 @@ $fortune = @implode("\n", $o);
 
 $tpl = array();
 $tpl['quote'] = preg_replace('/\s--\s?/', ' - ', $fortune);
+$tpl['quote'] = str_replace("\n",' ', $tpl['quote']);
+$tpl['quote'] = preg_replace('/\s+/', ' ', $tpl['quote']);
 
 require 'tpl.xml.php';
